d6db0ef6a6da5fe5f3f8b9047ea514b1df6e3074
Thithibaud <Thibaud.perez@gmail.com>
2023-05-19 16:49:05 +0200

    trying to get manual mode multy working

---
+++ test_manual_mode_multiV3.py
@@ -0,0 +1,158 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.animation as animation
+import comunicacion as com
+import os
+import csv
+
+# Global variables
+filename = os.environ.get("data_config_filename")
+if not ((filename is not None) and os.path.exists(filename)):
+    filename = filedialog.askopenfilename(filetypes=[('CSV Files', '*.csv')])
+
+if filename:
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+        try:
+            num_sensors = int(data[4][1])  # Assuming the number is stored in row 5, column 2
+        except ValueError:
+            print("Invalid number of sensors in the CSV file.")
+
+# Create an instance of Control_FlowBus
+mfc = com.Control_FlowBus('/dev/ttyUSB0')
+
+# Create a list to hold the nodes
+nodes = []
+for i in range(6, 6 + num_sensors):
+    nodes.append(data[i][0])
+print(nodes)
+
+# Initialize the graph data
+x_data = {}
+y_data = {}
+setpoint_data = {}
+measurement = {}
+setpoint = {}
+measurement_label = {}
+setpoint_slider = {}
+current_setpoint_label = {}
+desired_setpoint_label = {}
+
+# Create a figure and axis for the graph
+fig, ax = plt.subplots()
+graph_ax = {}
+
+# Create the measurement labels and setpoint sliders
+line = {}
+setpoint_line = {}
+for node in nodes:
+    line[node], = ax.plot([], [], label=f"Sensor {node}")
+    setpoint_line[node], = ax.plot([], [], label=f"Setpoint {node}")
+
+def update_measurement(node):
+    # Get the current measurement and setpoint
+    measurement[node] = mfc.get_mesure(str(node))
+    setpoint[node] = mfc.get_setpoint(str(node))
+
+    try:
+        # Convert measurement and setpoint to float
+        measurement_float = float(measurement[node])
+        setpoint_float = float(setpoint[node])
+
+        # Update the measurement label and setpoint slider
+        measurement_label[node].config(text=f"Measurement: {measurement_float}")
+        setpoint_slider[node].set(setpoint_float)
+
+        # Update the setpoint labels
+        current_setpoint_label[node].config(text=f"Current Setpoint: {setpoint_float:.2f}")
+        desired_setpoint_label[node].config(text=f"Desired Setpoint: {setpoint_slider[node].get():.2f}")
+
+        # Update the graph data
+        if node not in x_data:
+            x_data[node] = []
+            y_data[node] = []
+            setpoint_data[node] = []
+        x_data[node].append(len(x_data[node]) + 1)
+        y_data[node].append(measurement_float)
+        setpoint_data[node].append(setpoint_float)
+
+        # Update the graph lines
+        line[node].set_data(x_data[node], y_data[node])
+        setpoint_line[node].set_data(x_data[node], setpoint_data[node])
+
+        # Redraw the graph
+        canvas.draw()
+    except ValueError:
+        print(f"Invalid measurement or setpoint value for node {node}")
+
+    # Schedule the next update
+    root.after(1000, update_measurement, node)
+
+def set_setpoint(node, setpoint):
+    setpoint = float(setpoint)  # Convert setpoint to a float
+    mfc.send_setpoint(str(node), setpoint)
+
+def create_interface(node):
+    # Create a frame for the measurement section
+    measurement_frame = ttk.Frame(root, padding=10)
+    measurement_frame.pack()
+
+    # Create a label to display the measurement
+    measurement_label[node] = ttk.Label(measurement_frame, text="Measurement: ")
+    measurement_label[node].pack()
+
+    # Get the capacity and unit
+    capacity = mfc.get_capacity(str(node))
+    unit = mfc.get_unit(str(node))
+
+    # Create a label to display the capacity and unit
+    capa_unit_label = ttk.Label(measurement_frame, text=f"Capacity: {capacity} {unit}")
+    capa_unit_label.pack()
+
+    # Create a frame for the setpoint section
+    setpoint_frame = ttk.Frame(root, padding=10)
+    setpoint_frame.pack()
+
+    # Create a label and slider for the setpoint
+    setpoint_label = ttk.Label(setpoint_frame, text="Setpoint: ")
+    setpoint_label.pack(side=tk.LEFT)
+    setpoint_slider[node] = ttk.Scale(setpoint_frame, from_=0, to=100, orient=tk.HORIZONTAL, length=300,
+                                     command=lambda value, node=node: set_setpoint(node, value))
+    setpoint_slider[node].pack(side=tk.LEFT)
+
+    # Create a frame for the setpoint labels
+    setpoint_labels_frame = ttk.Frame(root, padding=10)
+    setpoint_labels_frame.pack()
+
+    # Create labels for the current and desired setpoints
+    current_setpoint_label[node] = ttk.Label(setpoint_labels_frame, text="Current Setpoint: ")
+    current_setpoint_label[node].pack()
+
+    desired_setpoint_label[node] = ttk.Label(setpoint_labels_frame, text="Desired Setpoint: ")
+    desired_setpoint_label[node].pack()
+
+    # Add the graph lines to the legend
+    ax.legend()
+
+# Create the main window
+root = tk.Tk()
+root.title("Mass Flow Sensor Configuration")
+
+# Embed the graph in the Tkinter window
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.get_tk_widget().pack()
+
+# Create the measurement labels and setpoint sliders
+for node in nodes:
+    create_interface(node)
+
+# Start the measurement update loop
+for node in nodes:
+    update_measurement(node)
+
+# Start the Tkinter event loop
+root.mainloop()
