205608bc4271dfcb0016b4a2d1f6421bd6f48e7c
Thithibaud <Thibaud.perez@gmail.com>
2023-05-29 14:13:47 +0200

    added shebang

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import tkinter as tk
 import tkinter.ttk as ttk
 import os
--- select_mfc.py
+++ select_mfc.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import csv
 import tkinter as tk
 import tkinter.ttk as ttk
